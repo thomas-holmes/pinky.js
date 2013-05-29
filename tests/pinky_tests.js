3ccac50b5cc576f9ce865a9641ced533b1d89cab
Thomas Holmes <thomas@devminded.com>
2013-05-28 21:24:40 -0400

    initial commit

---
+++ tests/pinky_tests.js
@@ -0,0 +1,7 @@
+var assert = require("assert")
+var pinky = require("../lib/pinky")
+describe('Pinky.js', function() {
+  it('exists', function() {
+    assert.equal(typeof(Pinky), 'object')
+  });
+});
