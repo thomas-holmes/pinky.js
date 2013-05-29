0d7c36671962385e1476d260e27fca7caf8003c2
Thomas Holmes <thomas@devminded.com>
2013-05-28 21:51:53 -0400

    Add shell of promise

--- tests/pinky_tests.js
+++ tests/pinky_tests.js
@@ -4,4 +4,16 @@ describe('Pinky.js', function() {
   it('exists', function() {
     assert.equal(typeof(Pinky), 'object')
   });
+
+  describe('Promises', function() {
+    it('can make a promise', function() {
+      var p = new Pinky.Promise;
+      assert.equal(Object.getPrototypeOf(p), Pinky.Promise.prototype);
+    });
+
+    it('take two arguments', function() {
+      var p = new Pinky.Promise;
+      assert.equal(p.then.length, 2)
+    });
+  });
 });
